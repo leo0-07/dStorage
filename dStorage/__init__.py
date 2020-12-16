dc6d8467c1715acb8954c8cff371b160bb28d19a
Leonardo de A. Lima <leonardo@asl-sl.com.br>
2020-12-16 15:34:45 -0300

    Correção na descrição da classe, Added version number, and classe description.

--- dStorage/__init__.py
+++ dStorage/__init__.py
@@ -1,3 +1,4 @@
-''' these is the constructor of class '''
+''' these class is builded to give all subclasses  one structure
+to make GUI interface and console interface generation dinamicaly.'''
 ''' Está é a função construtora da classe'''
 __version__ = "0.0.1a0"
