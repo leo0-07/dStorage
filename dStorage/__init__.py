fc9d5e58d6985f45030a5702862e0f12ee8d2739
Leonardo de A. Lima <leonardo@asl-sl.com.br>
2021-03-05 10:19:14 -0300

    adicionadas informações de licensa.

--- dStorage/__init__.py
+++ dStorage/__init__.py
@@ -1,5 +1,13 @@
-''' these class is builded to give all subclasses  one structure
-to make GUI interface and console interface generation dinamicaly.'''
-''' Classe para abstração e controle de objetos e banco de dados
+''' This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    dStorage  Copyright (C) 2021  Leonardo de Araújo Lima.
+                                    (leonardo@asl-sl.com.br)
+    This program comes with ABSOLUTELY NO WARRANTY
+    This is free software, and you are welcome to redistribute it *\
+these class is builded to give all subclasses  one structure
+to make GUI interface and console interface generation dinamicaly.
+Classe para abstração e controle de objetos e banco de dados
 associados aos mesmos '''
 __version__ = "0.0.1a5"
