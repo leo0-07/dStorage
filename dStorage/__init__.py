824abe10b926ad1eac0f8b60090c09c788e9f8c5
Leonardo de A. Lima <leonardo@asl-sl.com.br>
2021-03-14 10:44:33 -0300

    correção de declaração de licença

--- dStorage/__init__.py
+++ dStorage/__init__.py
@@ -1,13 +1,22 @@
-''' This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    dStorage  Copyright (C) 2021  Leonardo de Araújo Lima.
-                                    (leonardo@asl-sl.com.br)
+'''    dStorage, biblioteca para construção de aplicativos em python.
+#    Copyright (C) 2018, 2020–2021  Leonardo de Araújo Lima <leonardo@asl-sl.com.br>
+#    Copyright (C) 1983, 1994–1995, 1997, 2005, 2007, 2015  Leonardo de Araújo Lima
+#                                             <mailto:leonardo@asl-sl.com.br>
+#                                               <xmpp:linux77@suchat.org>
+#                               <https://linux77.asl-sl.com.br>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.  
     This program comes with ABSOLUTELY NO WARRANTY
-    This is free software, and you are welcome to redistribute it *\
-these class is builded to give all subclasses  one structure
-to make GUI interface and console interface generation dinamicaly.
-Classe para abstração e controle de objetos e banco de dados
-associados aos mesmos '''
+    This is free software, and you are welcome to redistribute it '''
 __version__ = "0.0.1a5"
