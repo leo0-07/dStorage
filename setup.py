d52bfdd195f25e870548d7e42e3eb55ba50d64f4
Leonardo de Araújo Lima <leonardo@debian-BULLSEYE-live-builder-AMD64>
2022-11-28 10:19:22 -0300

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 setup(name='dStorage',
-      version='1.0b',
-      py_modules=['dStorage'],
+        version='1.0b',
+        py_modules=['dStorage'],
+        AUTHOR = u'Leonardo de Araujo Lima',
+        EMAIL = 'feraleomg@gmail.com',
+        LICENSE = 'GPL3',
+        URL = 'https://pypi.python.org/project/dStorage/'
 )
-
