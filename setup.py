cce2db99fbccd4ff491642681c7560d7ee6760b6
Leonardo de Araújo Lima <leonardo@debian-BULLSEYE-live-builder-AMD64>
2023-04-16 13:58:15 -0300

    project structure revision 1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import find_packages
-import os
 from distutils.core import setup
+import os
+from setuptools import find_packages
 
 # User-friendly description from README.md
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -17,24 +17,24 @@ setup(
 	packages=find_packages('.'),
 	# Start with a small number and increase it with 
 	# every change you make https://semver.org 
-	version='1.0.0',
+	version='1.1.3rc1',
 	# Chose a license from here: https: // 
 	# help.github.com / articles / licensing - a - 
 	# repository. For example: MIT 
-	license='',
+	license='GPL3',
 	# Short description of your library 
-	description='',
+	description='this library make databases dynamic interfaces  creation',
 	# Long description of your library 
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	# Your name 
-	author='',
+	author='Leonardo de Araújo Lima',
 	# Your email 
-	author_email='',
+	author_email='feraleomg@gmail.com',
 	# Either the link to your github or to your website 
 	url='',
 	# Link from which the project can be downloaded 
-	download_url='',
+	download_url='https://github.com/leo0-07/dStorage',
 	# List of keywords 
 	keywords=[],
 	# List of packages to install with this one 
